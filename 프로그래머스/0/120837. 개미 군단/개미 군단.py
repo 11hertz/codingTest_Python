2a80df439a004ea3dccec7a75a0cf51cffbdedba
KIHOON LEE <reachzin@naver.com>
2024-04-29 20:42:43 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,7 +1,2 @@
 def solution(hp):
-    gen = hp // 5;
-    hp %= 5;
-    sol = hp // 3;
-    hp %= 3;
-    work = hp;
-    return gen + sol + work
+    return hp // 5 + ((hp % 5) // 3) + ((hp % 5) % 3)
