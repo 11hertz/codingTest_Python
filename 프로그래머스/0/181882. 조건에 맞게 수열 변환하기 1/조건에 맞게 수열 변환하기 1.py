365e304838708b39ee427cc82b635ab9e3a521d2
KIHOON LEE <reachzin@naver.com>
2024-05-01 01:18:17 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 26.25 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,7 +1,7 @@
 def solution(arr):
     for x in range(len(arr)):
-        if arr[x] % 2 == 0 and arr[x] >= 50:
-            arr[x] /= 2
-        elif arr[x] % 2 == 1 and arr[x] < 50:
+        if arr[x] >= 50 and not arr[x] % 2:
+            arr[x] //= 2
+        elif arr[x] < 50 and arr[x] % 2:
             arr[x] *= 2
     return arr
