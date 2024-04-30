dfe3fe207b9d6fe6bb5fa0058383ceae9e5c9db0
KIHOON LEE <reachzin@naver.com>
2024-05-01 01:31:56 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,3 +1,2 @@
 def solution(todo_list, finished):
-    answer = [todo_list[x] for x in range(len(finished)) if not finished[x]]
-    return answer
+    return [todo_list[x] for x in range(len(finished)) if not finished[x]]
