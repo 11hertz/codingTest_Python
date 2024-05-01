33be5178bfdb741d1573426bce868ff0e63eadbf
KIHOON LEE <reachzin@naver.com>
2024-05-01 23:28:34 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,9 +1,5 @@
 def solution(num_list):
-    odd = 0
-    even = 0
+    answer = [0,0]
     for x in num_list:
-        if x % 2 == 0:
-            even += 1
-        else:
-            odd += 1
-    return [even, odd]
+        answer[x % 2] += 1
+    return answer
