609966ac21c260ef74c750e53494aa6aa4c7a620
KIHOON LEE <reachzin@naver.com>
2024-05-02 01:14:51 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,5 +1,2 @@
 def solution(arr, delete_list):
-    answer = []
-    for x in arr:
-        if x not in delete_list : answer.append(x)
-    return answer
+    return [x for x in arr if x not in delete_list]
