7ceaef2b289f46f3624b86b02e2c0a301ef3e656
KIHOON LEE <reachzin@naver.com>
2024-05-03 02:34:32 +0900

    [level 0] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = []
-    for x in range(len(my_string)):
-        answer.append(my_string[x:])
-    answer.sort()
-    return answer
+    return sorted(my_string[x:] for x in range(len(my_string)))
