deb7498079a2bb0838e8860cff9f35d86cf4f172
KIHOON LEE <reachzin@naver.com>
2024-05-03 02:29:24 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,5 +1,4 @@
 def solution(arr, intervals):
-    answer = []
-    for x in range(len(intervals)):
-        answer += arr[intervals[x][0] : intervals[x][1] + 1]
-    return answer
+    S1, E1 = intervals[0]
+    S2, E2 = intervals[1]
+    return arr[S1 : E1 + 1] + arr[S2 : E2 + 1]
