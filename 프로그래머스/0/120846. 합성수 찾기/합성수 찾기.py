a65874f7460231b25b49ef33623009ee312b60d7
KIHOON LEE <reachzin@naver.com>
2024-05-04 04:53:15 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    
+    def getDivisor(num):
+        count = 0
+        for x in range(1, num + 1):
+            if num % x == 0 : count += 1
+        return count
+    
+    for x in range(1, n + 1):
+        divisorCount = 0
+        if getDivisor(x) >= 3 : answer += 1
+        
+    return answer
