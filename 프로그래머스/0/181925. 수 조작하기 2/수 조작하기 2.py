b873d4d680c6cff31a6c21f439a911a7a13d4093
KIHOON LEE <reachzin@naver.com>
2024-05-03 18:49:11 +0900

    [level 0] Title: 수 조작하기 2, Time: 12.67 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,11 +1,12 @@
 def solution(numLog):
     answer = ''
     for x in range(0, len(numLog) - 1):
-        if numLog[x + 1] - numLog[x] == 1:
+        calc = numLog[x + 1] - numLog[x]
+        if calc == 1:
             answer += 'w'
-        elif numLog[x + 1] - numLog[x] == -1:
+        elif calc == -1:
             answer += 's'
-        elif numLog[x + 1] - numLog[x] == 10:
+        elif calc == 10:
             answer += 'd'
         else : answer += 'a'
     return answer
