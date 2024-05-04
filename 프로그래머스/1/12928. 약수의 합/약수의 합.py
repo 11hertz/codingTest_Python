866d6c6d8af0331521e26902492f887931612ca7
KIHOON LEE <reachzin@naver.com>
2024-05-05 01:52:16 +0900

    [level 1] Title: 약수의 합, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,6 +1,2 @@
 def solution(n):
-    divisorList = []
-    for x in range(1, n + 1):
-        if(n % x == 0) : divisorList.append(x)
-        
-    return sum(divisorList)
+    return sum(x for x in range(1, n + 1) if n % x == 0)
