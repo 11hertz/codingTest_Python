b5b60a17e1076a39cf20e8d395b5ff34c1d61eb7
KIHOON LEE <reachzin@naver.com>
2024-05-06 05:24:37 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,2 @@
 def solution(my_string, m, c):
-    answer = ''
-    for x in range(c - 1, len(my_string)):
-        if x % m - c + 1 == 0 : answer += my_string[x]
-    return answer
+    return my_string[c - 1 : : m]
