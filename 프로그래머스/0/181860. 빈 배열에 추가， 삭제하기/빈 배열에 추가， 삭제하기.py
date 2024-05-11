fffe7e583797c0582485462845bbc2d798184c7d
KIHOON LEE <reachzin@naver.com>
2024-05-11 19:20:24 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -0,0 +1,9 @@
+def solution(arr, flag):
+    answer = []
+    
+    for x in range(len(flag)):
+        if flag[x] : answer += [arr[x]] * (arr[x] * 2)
+        else :
+            for x in range(arr[x]) : answer.pop()
+    
+    return answer
