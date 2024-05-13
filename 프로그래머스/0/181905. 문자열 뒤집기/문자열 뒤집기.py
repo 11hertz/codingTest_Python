af7e98db7daf9ea7a7e1e9502a09841ec0ed0384
KIHOON LEE <reachzin@naver.com>
2024-05-13 23:55:34 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,5 +1,2 @@
 def solution(my_string, s, e):
-    answer = ''.join(list(my_string[0 : s: 1])) + ''.join(reversed(list(my_string[s : e + 1: 1]))) + ''.join(list(my_string[e + 1::]))
-    
-    
-    return answer
+    return my_string[:s] + ''.join(reversed(my_string[s : e + 1])) + my_string[e + 1:]
