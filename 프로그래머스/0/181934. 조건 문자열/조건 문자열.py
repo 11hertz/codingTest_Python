fc2791178b1b754718811fa8cb4c84b627379e83
KIHOON LEE <reachzin@naver.com>
2024-05-24 04:39:50 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,5 +1,2 @@
 def solution(ineq, eq, n, m):
-    answer = 0
-    if eq == '!' : eq = ''
-
-    return int(eval(str(n) + ineq + eq + str(m)))
+    return int(eval(str(n) + ineq + eq.replace('!', '') + str(m)))
