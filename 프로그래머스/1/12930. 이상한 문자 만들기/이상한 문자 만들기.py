23634cae15bdbfba177b6b002f1741faec78d595
KIHOON LEE <reachzin@naver.com>
2024-05-29 09:10:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = ''
+    
+    count = 0
+    for x in s :
+        if x == ' ' :
+            count = 0
+            answer += ' '
+            continue
+        if count % 2 == 0 : answer += x.upper()
+        else : answer += x.lower()
+        count += 1
+        
+    
+    return answer
