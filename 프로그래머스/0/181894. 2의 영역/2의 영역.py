edb1904328b1f9f4204f5fb7ed2ddb83ddf6fd4f
KIHOON LEE <reachzin@naver.com>
2024-06-03 00:53:52 +0900

    [level 0] Title: 2의 영역, Time: 0.62 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,12 +1,4 @@
 def solution(arr):
-    fIdx = -1
-    lIdx = -1
-    
-    for idx, x in enumerate(arr):
-        if x == 2 and fIdx == -1 : fIdx = idx
-        elif x == 2 and fIdx != -1 : lIdx = idx
-    
-    if fIdx != -1 and lIdx != -1 : return arr[fIdx : lIdx + 1]
-    elif fIdx != -1 and lIdx == -1 : return [arr[fIdx]]
-    
-    return [-1]
+    if 2 not in arr:
+        return [-1]
+    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
