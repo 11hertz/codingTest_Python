7693e615af2818dc34f2cd55cb4aa0d351a86f3e
KIHOON LEE <reachzin@naver.com>
2024-06-03 01:20:52 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,11 @@
+def solution(k, m, score):
+    answer = 0
+    
+    score.sort(reverse = True)
+    
+    for x in range(0, len(score), m):
+        if len(score[x : x + m]) == m :
+            answer += min(score[x : x + m]) * len(score[x : x + m])
+        
+    
+    return answer
