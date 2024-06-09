14b0ef3c7ae2095eb8ad8af169f1a74ed517e296
KIHOON LEE <reachzin@naver.com>
2024-06-09 23:19:59 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -2,11 +2,11 @@ def solution(chicken):
     answer = 0;
     coupon = chicken;
     count = 0
+    
     while count < len(str(chicken)): 
         answer += int(coupon / 10);
         res = coupon % 10;
         coupon = int(coupon / 10) + res
         count += 1
-    
     
     return answer;
