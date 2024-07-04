528cd87f2c85d6315a20abb781efdb94654109b5
KIHOON LEE <reachzin@naver.com>
2024-07-04 16:24:55 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,9 +1,5 @@
 def solution(common):
-    answer = 0
-    
     A, B, C = common[0 : 3]
     
     if B - A == C - B : return common[-1] + (B - A)
     else : return common[-1] * (B // A)
-    
-    return answer
